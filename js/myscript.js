f4f874563d156db2c5975c9fcdb5afd71cbfde4d
PalumboAndrea <andreaseiuno.ap@gmail.com>
2022-12-14 15:03:50 +0100

    add MILESTONE 1 - to do list and text decoration

--- js/myscript.js
+++ js/myscript.js
@@ -3,7 +3,38 @@ const { createApp } = Vue;
 createApp({
     data()  {
         return {
-            message:'Hello Vue!'
+            todo : [
+                {
+                    text: 'Fare la lavatrice' ,
+                    done: true,
+                },
+                {
+                    text: 'Fare la lavastoviglie' ,
+                    done: false,
+                },
+                {
+                    text: 'Fare la doccia' ,
+                    done: false,
+                },
+                {
+                    text: 'Fare i compiti' ,
+                    done: false,
+                },
+                {
+                    text: 'Andare a giocare a tennis' ,
+                    done: false,
+                },
+
+            ]
         }
+    },
+    methods: {
+
     }
+
+
+
+
+
+
 }).mount('#app')
